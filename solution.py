2fd8d3e3a466513484e5f424f10e245f3de5b6cd
Edgar Hernandez <edhzsz@gmail.com>
2017-01-24 17:32:59 +0100

    code: Solve the diagonal sudoku Just adds both diagonals as units and solve with the normal Sudoku solver.

--- solution.py
+++ solution.py
@@ -13,7 +13,8 @@ boxes = cross(ROWS, COLS)
 row_units = [cross(r, COLS) for r in ROWS]
 column_units = [cross(ROWS, c) for c in COLS]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
+diagonal_units = [[a+b for a,b in zip(ROWS, COLS)], [a+b for a,b in zip(ROWS, COLS[::-1])]]
+unitlist = row_units + column_units + square_units + diagonal_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
