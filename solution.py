897a99494436cf564bc9c7f7ce86974ee423532f
Edgar Hernandez <edhzsz@gmail.com>
2017-01-27 16:36:40 +0100

    code: Reenable the diagonal sudoku solver

--- solution.py
+++ solution.py
@@ -15,7 +15,7 @@ row_units = [cross(r, COLS) for r in ROWS]
 column_units = [cross(ROWS, c) for c in COLS]
 square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
 diagonal_units = [[a+b for a, b in zip(ROWS, COLS)], [a+b for a, b in zip(ROWS, COLS[::-1])]]
-unitlist = row_units + column_units + square_units #+ diagonal_units
+unitlist = row_units + column_units + square_units + diagonal_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s], []))-set([s])) for s in boxes)
 
